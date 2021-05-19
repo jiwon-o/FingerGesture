465dfb91194b2072bc3dfd0f0d29b1e9ea61c960
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-05-19 18:46:24 +0900

    손 제스처 1, 2, 3, 4, 5 인식

--- FingerGesture.py
+++ FingerGesture.py
@@ -23,6 +23,62 @@ with mp_hands.Hands(
 
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
+                # finger statement
+                thumb = False
+                index = False
+                middle = False
+                ring = False
+                pinky = False
+
+                pseudoFixKeyPoint = hand_landmarks.landmark[2].x
+                if (hand_landmarks.landmark[3].x < pseudoFixKeyPoint and hand_landmarks.landmark[4].x < pseudoFixKeyPoint):
+                    thumb = True
+
+                pseudoFixKeyPoint = hand_landmarks.landmark[6].y
+                if (hand_landmarks.landmark[7].y < pseudoFixKeyPoint and hand_landmarks.landmark[8].y < pseudoFixKeyPoint):
+                    index = True
+
+                pseudoFixKeyPoint = hand_landmarks.landmark[10].y
+                if (hand_landmarks.landmark[11].y < pseudoFixKeyPoint and hand_landmarks.landmark[12].y < pseudoFixKeyPoint):
+                    middle = True
+
+                pseudoFixKeyPoint = hand_landmarks.landmark[14].y
+                if (hand_landmarks.landmark[15].y < pseudoFixKeyPoint and hand_landmarks.landmark[16].y < pseudoFixKeyPoint):
+                    ring = True
+
+                pseudoFixKeyPoint = hand_landmarks.landmark[18].y
+                if (hand_landmarks.landmark[19].y < pseudoFixKeyPoint and hand_landmarks.landmark[20].y < pseudoFixKeyPoint):
+                    pinky = True
+
+                if (thumb and index and middle and ring and pinky):
+                    cv2.putText(
+                        image, text='Five', org=(10, 30),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=255, thickness=2)
+
+                if (not thumb and index and middle and ring and pinky):
+                    cv2.putText(
+                        image, text='Four', org=(10, 30),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=255, thickness=2)
+
+                if (not thumb and index and middle and ring and not pinky):
+                    cv2.putText(
+                        image, text='Three', org=(10, 30),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=255, thickness=2)
+
+                if (not thumb and index and middle and not ring and not pinky):
+                    cv2.putText(
+                        image, text='Two', org=(10, 30),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=255, thickness=2)
+
+                if (not thumb and index and not middle and not ring and not pinky):
+                    cv2.putText(
+                        image, text='One', org=(10, 30),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                        color=255, thickness=2)
 
                 mp_drawing.draw_landmarks(
                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
