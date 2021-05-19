c6aaa89b79e936c03d3d2ec0b4b3e5bf0bba8531
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-05-19 18:35:18 +0900

    손가락 제스처 인식 from mediapipe

---
+++ FingerGesture.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(
+    max_num_hands=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            continue
+
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow('image', image)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
